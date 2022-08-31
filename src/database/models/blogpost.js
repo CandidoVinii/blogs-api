24fb0c812e330e761be9ceb7eed4e1fb202e1ddf
CandidoVinii <vinicius.dias2020@engenharia.ufjf.br>
2022-08-31 16:13:26 -0300

    feat: implementado requisito 10

--- src/database/models/blogpost.js
+++ src/database/models/blogpost.js
@@ -1,34 +1,62 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
-  class BlogPost extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-      BlogPost.belongsTo(models.User, {
-        foreignKey: 'userId'
-      })
-      BlogPost.hasMany(models.PostCategories, {
-        foreignKey: 'postId'
-      })
-    }
-  };
-  BlogPost.init({
+// 'use strict';
+// const {
+//   Model
+// } = require('sequelize');
+// module.exports = (sequelize, DataTypes) => {
+//   class BlogPost extends Model {
+//     /**
+//      * Helper method for defining associations.
+//      * This method is not a part of Sequelize lifecycle.
+//      * The `models/index` file will call this method automatically.
+//      */
+//     static associate(models) {
+//       // define association here
+//       BlogPost.belongsTo(models.User, {
+//         foreignKey: 'userId'
+//       })
+//       BlogPost.hasMany(models.PostCategories, {
+//         foreignKey: 'postId'
+//       })
+//     }
+//   };
+//   BlogPost.define({
+//     title: DataTypes.STRING,
+//     content: DataTypes.STRING,
+//     userId: DataTypes.INTEGER,
+//     published: DataTypes.DATE,
+//     updated: DataTypes.DATE
+//   }, {
+//     sequelize,
+//     modelName: 'BlogPost',
+//     timestamps: false,
+//   });
+//   return BlogPost;
+// };
+
+const BlogPost = (sequelize, DataTypes) => {
+  const BlogPost = sequelize.define("BlogPost", {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
     title: DataTypes.STRING,
     content: DataTypes.STRING,
     userId: DataTypes.INTEGER,
     published: DataTypes.DATE,
     updated: DataTypes.DATE
-  }, {
-    sequelize,
-    modelName: 'BlogPost',
-    timestamps: false,
-  });
+  }, { timestamps: false });
+
+  BlogPost.associate = (models) => {
+    BlogPost.belongsTo(models.User, {
+      foreignKey: 'userId'
+    })
+    BlogPost.hasMany(models.PostCategories, {
+      foreignKey: 'postId'
+    });
+  }
+
   return BlogPost;
 };
+
+module.exports = BlogPost;

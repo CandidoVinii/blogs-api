e92163cd96c97348c2de3f907ae1a06a127aa141
CandidoVinii <vinicius.dias2020@engenharia.ufjf.br>
2022-12-03 20:12:27 -0300

    refactor: trocado forma de criar o model para tentar achar o erro de vinculação com outra tabela 1:n

--- src/database/models/blogpost.js
+++ src/database/models/blogpost.js
@@ -1,38 +1,3 @@
-// 'use strict';
-// const {
-//   Model
-// } = require('sequelize');
-// module.exports = (sequelize, DataTypes) => {
-//   class BlogPost extends Model {
-//     /**
-//      * Helper method for defining associations.
-//      * This method is not a part of Sequelize lifecycle.
-//      * The `models/index` file will call this method automatically.
-//      */
-//     static associate(models) {
-//       // define association here
-//       BlogPost.belongsTo(models.User, {
-//         foreignKey: 'userId'
-//       })
-//         BlogPost.hasMany(models.PostCategories, {
-//         foreignKey: 'postId'
-//       })
-//     }
-//   };
-//   BlogPost.define({
-//     title: DataTypes.STRING,
-//     content: DataTypes.STRING,
-//     userId: DataTypes.INTEGER,
-//     published: DataTypes.DATE,
-//     updated: DataTypes.DATE
-//   }, {
-//     sequelize,
-//     modelName: 'BlogPost',
-//     timestamps: false,
-//   });
-//   return BlogPost;
-// };
-
 const blogPost = (sequelize, DataTypes) => {
   const blogPost = sequelize.define('BlogPost', {
       id: {

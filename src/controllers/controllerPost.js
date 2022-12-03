0a7c7f3b4742c5c9159712e2f03ed8ae4b3fcb53
CandidoVinii <vinicius.dias2020@engenharia.ufjf.br>
2022-12-03 20:11:51 -0300

    feat: criado funcionalidade de buscar, deletar, atualizar e criar um novo post

--- src/controllers/controllerPost.js
+++ src/controllers/controllerPost.js
@@ -1,12 +1,36 @@
-const { getAllPosts, postById } = require('../services/post');
+const { getAllPosts, postById, createPost, searchPost, deletePost, updatePost } = require('../services/post');
 
-// const postCreator = async (req, res) => {
-//     const { title, content, categoryIds } = req.body;
+const postCreator = async (req, res) => {
+    const token = req.header('Authorization');
 
-//     const response = await createPost({ title, content, categoryIds });
+    const response = await createPost(req.body, token);
+    return res.status(201).send(response);
+};
 
-//     return res.status(201).send(response);
-// };
+const postSearch = async (req, res) => {
+    const { q } = req.query
+    const response = await searchPost(q);
+    if (!response) return res.status(404).send({ message: 'Post not found' });
+    return res.status(200).send(response);
+};
+
+const postDelete = async (req, res) => {
+    const { id } = req.params;
+    const token = req.header('Authorization');
+
+    const response = await deletePost(id, token);
+    if(!response) return res.status(404).send({ message: 'Post not found' });
+    return res.status(204).end();
+};
+
+const postUpdate = async (req, res) => {
+    const { id } = req.params;
+    const token = req.header('Authorization');
+    const { title, content }= req.body;
+    const response = await updatePost(id, token, { title, content });
+    if(!response) return res.status(404).send({ message: 'Post not found' });
+    return res.status(200).send(response);
+}
 
 const getAllBlogsPosts = async (_req, res) => {
     const response = await getAllPosts();
@@ -25,4 +49,4 @@ const getPostById = async (req, res) => {
     return res.status(200).send(response);
 };
 
-module.exports = { getAllBlogsPosts, getPostById };
+module.exports = { getAllBlogsPosts, getPostById, postCreator, postSearch, postDelete, postUpdate };
--- src/services/post.js
+++ src/services/post.js
@@ -1,10 +1,24 @@
-const { User, BlogPost, Category } = require('../database/models');
+const { User, BlogPost, Category, PostCategory, sequelize } = require('../database/models');
+const jwt = require('jsonwebtoken');
+const { Op } = require('sequelize');
 
-// const createPost = async ({ title, content, categoryIds }) => {
-//     const response = await BlogPost.create({ title, content, categoryIds });
-
-//     return response;
-// };
+const createPost = async (data, token) => {
+    const decodeToken = jwt.decode(token, { complete: true });
+    const id = decodeToken.payload.id;
+    const response = await sequelize.transaction(async (t) => {
+        const post = await BlogPost.create(
+            { title: data.title, content: data.content, userId: id },
+            { transaction: t },
+        );
+        const categories = data.categoryIds.map((getId) => ({ postId: post.id, categoryId: getId }));
+        await PostCategory.bulkCreate(
+            categories,
+            { transaction: t },
+        );
+        return post;
+    });
+    return response;
+};
 
 const getAllPosts = async () => {
     const response = await BlogPost.findAll({
@@ -31,4 +45,44 @@ const postById = async ({ id }) => {
     return findOne;
 };
 
-module.exports = { getAllPosts, postById };
+const searchPost = async (searchString) => {
+    const search = await BlogPost.findAll({
+        where: {
+            [Op.or]: [
+                { title: { [Op.substring]: searchString } },
+                { content: { [Op.substring]: searchString } },
+            ],
+        },
+        include: [
+            { model: User, as: 'user', attributes: { exclude: ['password'] } },
+            { model: Category, as: 'categories', through: { attributes: [] } },
+        ]
+    });
+    if(search.length === 0) return false;
+    return search;
+}
+
+const deletePost = async (postId, token) => {
+    const findPost = await BlogPost.findByPk(postId);
+    const decodeToken = jwt.decode(token, { complete: true });
+    const id = decodeToken.payload.id;
+    if(!findPost) return false;
+    if(findPost.userId !== id) return false;
+    await BlogPost.destroy({
+        where: { id: postId }
+    });
+};
+
+const updatePost = async (postId, token, { title, content }) => {
+    const findPost = await BlogPost.findByPk(postId);
+    const decodeToken = jwt.decode(token, { complete: true });
+    const id = decodeToken.payload.id;
+    if(!findPost) return false;
+    if(findPost.userId !== id) return false;
+    await BlogPost.update(
+        { title, content },
+        { where: { id: postId } },
+    );
+    return await BlogPost.findByPk(postId);
+}
+module.exports = { getAllPosts, postById, createPost, searchPost, deletePost, updatePost };
